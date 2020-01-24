fd54b5dfd06a7ba954bc497c595473337e09f2db
abhianshu2020 <abhianshu2020@gmail.com>
2020-01-24 21:42:19 +0530

    lint fix

--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -5,7 +5,7 @@ import { PagesComponent } from './pages.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ECommerceComponent } from './e-commerce/e-commerce.component';
 import { NotFoundComponent } from './miscellaneous/not-found/not-found.component';
-import { HealthPredictorComponent } from './health-predictor/health-predictor.component';;
+import { HealthPredictorComponent } from './health-predictor/health-predictor.component';
 
 const routes: Routes = [{
   path: '',
