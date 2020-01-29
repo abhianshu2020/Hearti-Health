c58305e62982bd0677018dbe90d72dd4152e43b0
abhianshu2020 <abhianshu2020@gmail.com>
2020-01-29 10:08:08 +0530

    a

--- src/app/@theme/components/footer/footer.component.ts
+++ src/app/@theme/components/footer/footer.component.ts
@@ -3,13 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'ngx-footer',
   styleUrls: ['./footer.component.scss'],
-  // TODO:  Add Copyright Symbol in the Footer Template
-  // TODO: Add Share links for Twitter and LinkedIn
-  template: `    
+  // TODO: Add Copyright Symbol in the Footer Template
+  // TODO: Add Social Media Links for twitter and linked in.
+  // Classes required: 'ion ion-social-twitter','ion ion-social-linkedin'
+  template: `
+    <span class="created-by">&copy; <b><a href="#" target="_blank">SkillAssure</a></b> 2020</span>
     <div class="socials">
       <a href="#" target="_blank" class="ion ion-social-github"></a>
       <a href="#" target="_blank" class="ion ion-social-facebook"></a>
-      
     </div>
   `,
 })
